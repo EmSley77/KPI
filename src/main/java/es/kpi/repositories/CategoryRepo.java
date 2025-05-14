ca9610b7c07f22a87c18931967ce9ab5a5f5c5fb
EmSley77 <emanuelsleyman@gmail.com>
2025-05-14 18:57:27 +0200

    definition dto

--- src/main/java/es/kpi/repositories/CategoryRepo.java
+++ src/main/java/es/kpi/repositories/CategoryRepo.java
@@ -1,7 +1,15 @@
 package es.kpi.repositories;
 
 import es.kpi.entities.Category;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+
+
+    List<Category> findAllByUserId(@Size(max = 36) @NotNull String userId);
+    List<Category> findAllByUserIdAndName(String userId, String name);
 }
--- src/main/java/es/kpi/services/DefinitionService.java
+++ src/main/java/es/kpi/services/DefinitionService.java
@@ -1,19 +1,61 @@
 package es.kpi.services;
 
+import es.kpi.dto.request.CreateDefinitionDTO;
+import es.kpi.dto.response.CategoryResponseDTO;
+import es.kpi.entities.Category;
+import es.kpi.repositories.CategoryRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class DefinitionService {
 
+    private final CategoryRepo categoryRepo;
+
     //create
+    public void createDefinition(CreateDefinitionDTO createDefinitionDTO) {
+        categoryRepo.save(mapToDefinitionRequestDTO(createDefinitionDTO));
+    }
+
+    //map to category entity obj
+    public Category mapToDefinitionRequestDTO(CreateDefinitionDTO createDefinitionDTO) {
+        return new Category(
+                createDefinitionDTO.getName(),
+                createDefinitionDTO.getUserId()
+        );
+    }
 
     //edit
 
     //delete
+    public void deleteCategory(Long categoryId) {
+        categoryRepo.deleteById(categoryId);
+    }
 
     //fetch
+    public List<CategoryResponseDTO> getAllCategoriesByUserId(String userId) {
+        return categoryRepo.findAllByUserId(userId)
+                .stream()
+                .map(this::mapToResponseDTO)
+                .toList();
+    }
+
+    //fetch by name
+    public List<CategoryResponseDTO> getAllCategoriesByUserIdAndName(String userId, String name) {
+        return categoryRepo.findAllByUserIdAndName(userId, name)
+                .stream()
+                .map(this::mapToResponseDTO)
+                .toList();
+    }
+
+
+    public CategoryResponseDTO mapToResponseDTO(Category category) {
+        return new CategoryResponseDTO(category.getId(), category.getName(), category.getUserId());
+    }
 }
